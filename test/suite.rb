6b2107fe5474798cf97377f02cdd7f48d5e3a085
Tim Blair <tim@bla.ir>
2011-04-26 08:29:09 +0100

    Make test suite look in subfolders.

--- test/suite.rb
+++ test/suite.rb
@@ -1,3 +1,2 @@
 require File.expand_path('../helper', __FILE__)
-
-Dir[File.expand_path('../test_*.rb', __FILE__)].each { |f| require f }
+Dir[File.expand_path('../**/test_*.rb', __FILE__)].each { |f| require f }
