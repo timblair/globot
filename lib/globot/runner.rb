b9efc20d004923d336fdb9ed4c276f894b22b133
Tim Blair <tim@bla.ir>
2011-04-26 08:29:44 +0100

    Added a class-level logger to the runner.

--- lib/globot/runner.rb
+++ lib/globot/runner.rb
@@ -1,13 +1,31 @@
 require File.join(File.dirname(__FILE__), 'bot')
 require 'yaml'
+require 'logger'
 
 module Globot
+
+  class << self
+    attr_accessor :logger
+
+    def init_logger(file, level)
+      file = File.join(File.dirname(__FILE__), '..', '..', file) if file.class == String && file.match(/^[\\\/]/)
+      Globot.logger = Logger.new(file)
+      Globot.logger.progname = 'globot'
+      # TODO: pass the correct log level through from the config
+      Globot.logger.level = Logger::DEBUG
+    end
+  end
+
   class Runner
 
     def initialize(opts)
-      puts "Initing new runner with #{opts.inspect}"
       @config = YAML::load(File.read(opts[:config]))
-      puts "Config settings are #{@config.inspect}"
+
+      logger = (@config['globot']['logger'] || {}).merge({ 'path' => 'tmp/globot.log', 'level' => :info })
+      Globot.init_logger((opts[:daemonise] ? logger['path'] : STDOUT), logger['level'])
+
+      Globot.logger.debug "Initing new runner with #{opts.inspect}"
+      Globot.logger.debug "Config settings are #{@config.inspect}"
     end
 
     def start
@@ -29,4 +47,5 @@ module Globot
       puts "Might be running"
     end
   end
+
 end
