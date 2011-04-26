b3ea130ce1ad15bd022fffcb805d204701b923c2
Tim Blair <tim@bla.ir>
2011-04-26 08:31:59 +0100

    Added `!commands` to incoming messages, plus handled a couple of error cases for non-user initiated messages.

--- lib/globot/message.rb
+++ lib/globot/message.rb
@@ -1,25 +1,46 @@
 module Globot
   class Message
 
-    attr_accessor :body, :type, :person, :room
+    attr_accessor :raw, :command, :body, :type, :person, :room
 
     def initialize(msg, room)
-      puts msg.inspect
-      @body   = msg['body']
+      @raw    = msg['body']
       @type   = msg['type']
-      @person = msg['user']['name']
+      @person = msg['user'].nil? ? nil : msg['user']['name']
       @room   = room
+      @command, @body = self.class.parse_command(@raw)
     end
 
-    # define methods for each of the basic functions
+    # Define methods for each of the basic functions: each dynamically
+    # created method simply relays the given raw message body to the
+    # appropriate handler within the Room that this message came from.
+    # This means that we can only respond to a message in the same room
+    # that is originates from.
     %w(speak paste upload play).each do |cmd|
-      define_method cmd do |msg|
-        @room.send cmd, msg
+      define_method cmd do |body|
+        @room.send cmd, body
       end
     end
 
-    # another handy alternative for writing a new message
+    # We'll also alias the strangely-named `speak` method to something
+    # more sensible.
     alias :reply :speak
+
+    # Did this message contain a command?
+    def command?
+      !@command.nil?
+    end
+
+    # Parse out a command command name as provided within the raw
+    # message body, given by starting the message with an exclamation
+    # mark and the command name, follwed by the rest of the message:
+    #
+    #     !command_name rest of message
+    def self.parse_command(str)
+      !str.nil? && str.match(/^![^\s]/) ?
+        [ str[1..-1].split(/\s/).first, str.gsub(/^.*\s+/, '').downcase ] :
+        [ nil, str ]
+    end
 
   end
 end
