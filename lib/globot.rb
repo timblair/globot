3c8cea35abbef385aa3a8c5c6d6c6ac35b8ffa9a
Tim Blair <tblair@globalpersonals.co.uk>
2011-04-26 12:31:16 +0100

    Stop the damn annoying "peer certificate" SSL warning.

--- lib/globot.rb
+++ lib/globot.rb
@@ -1,10 +1,14 @@
 require 'rubygems'
 require 'tinder'
-require 'json'   # TODO: Tinder needs this internally for parsing the transcript
+require 'json'   # Tinder needs this internally for parsing the transcript
 require 'globot/bot'
 require 'globot/message'
 require 'globot/plugins'
 require 'globot/runner'
+
+# Turn SSL verification off to stop incredibly annoying "peer certificate
+# won't be verified in this SSL session" warnings.
+require 'ext/tinder/disable_ssl_verification'
 
 module Globot
   VERSION = "0.0.1"
